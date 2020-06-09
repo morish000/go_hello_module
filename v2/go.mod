8bf1732b818e2ed722e3ad6d56c2bef8511190f3
morish000 <morish@ga.w0w.jp>
2020-06-09 11:10:53 +0900

    v2

---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/morish000/go_hello_module/v2
+
+go 1.14
---
+++ v2/hello-world.go
@@ -0,0 +1,11 @@
+package go_hello_module
+
+import (
+	"fmt"
+	"github.com/morish000/go_hello_module/v2/hello"
+	"github.com/morish000/go_hello_module/v2/world"
+)
+
+func HelloWorld() string {
+	return fmt.Sprintf("%v %v %v", hello.Hello(), world.World(), "v2")
+}
