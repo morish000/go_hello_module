412e56c9794dc98750e3b9ff8b246c4f1e5695fc
morish000 <morish@ga.w0w.jp>
2020-06-09 11:34:34 +0900

    v2.1

--- v2/hello-world_test.go
+++ v2/hello-world_test.go
@@ -3,7 +3,7 @@ package go_hello_module
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-	want := "Hello World v2"
+	want := "Hello World v2.1"
 	if got := HelloWorld(); got != want {
 		t.Errorf("HelloWrold() = %q, want = %q", got, want)
 	}
